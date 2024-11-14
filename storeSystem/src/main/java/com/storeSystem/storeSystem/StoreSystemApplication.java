f4f48a76e22c0d959881d802225f296a7d4ad2c3
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-11-14 23:04:23 +0630

    start

---
+++ storeSystem/src/main/java/com/storeSystem/storeSystem/StoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.storeSystem.storeSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSystemApplication.class, args);
+	}
+
+}
